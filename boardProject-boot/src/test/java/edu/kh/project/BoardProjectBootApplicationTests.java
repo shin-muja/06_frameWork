c833b960a8c418b7b09fc96587959ae39fa12846
shin-muja <shin970712@gmail.com>
2025-01-13 10:21:57 +0900

    junit test 예제

--- boardProject-boot/src/test/java/edu/kh/project/BoardProjectBootApplicationTests.java
+++ boardProject-boot/src/test/java/edu/kh/project/BoardProjectBootApplicationTests.java
@@ -3,11 +3,17 @@ package edu.kh.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Spring Boot 테스트를 실행하기 위한 어노테이션
+// 스프링 애플리케이션 컨텍스트를 로드하고, 애클리케이션의 전체 환
+// 경을 테스트 할 수 있게 해줌
 @SpringBootTest
 class BoardProjectBootApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		// 애플리케이션 컨텍스트가 제대로 로드되는지 확인하는 기본 테스트를 수행 메서드
+		System.out.println("어플리케이션 컨텍스트 로드완료");
 	}
 
 }
